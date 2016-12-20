a8e4aa89ec56492997417fcf3a4a7f15b0edd0b8
Peter Ondrejka <pondrejk@redhat.com>
2016-12-20 11:07:36 +0100

    alt keys rearanged back

--- keymap.c
+++ keymap.c
@@ -48,17 +48,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|------+------+------+------+------|
  * | Shift|   Y  |   X  |   C  |   V  |   B  | Right|   N  |   M  |  ,?  |  .:  |  -_  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  GUI | Alt  | Numer|Lower | sp   | Enter| sp   |Raise | Left | Down |  Up  |
+ * | Numer| Ctrl |  GUI | Alt  |Lower | sp   | sp   | sp   |Raise | Ente | Down |  Up  |
  * `-----------------------------------------------------------------------------------'
  */
 
 [_QWERTY] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   S(KC_SCLN), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P },
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_LCBR,    KC_H,    KC_J,    KC_K,    KC_L,    KC_BSPC },
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_RGHT,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH },   
-  {KC_LCTL, KC_LGUI, KC_LALT, BACKLIT, LOWER,   KC_SPC, F(1),       KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP }
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_RGHT,    KC_N,    KC_M,    KC_COMM, KC_DOT,  F(1) },
+  {BACKLIT, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC, KC_SPC,     KC_SPC,  RAISE,   KC_ENT,  KC_DOWN, KC_UP }
 },
-
 
 /* Lower (SK)
  * ,-----------------------------------------------------------------------------------.
@@ -68,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|------+------+------+------+------|
  * |      |  ý   |      |  č   |      |      |      |  ň   |   [  |   ]  |   {  |   }  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      | Enter|      |      |      | pgdn | pgup |
+ * |      |      |      |      |      |      |      |      |      |      | pgdn | pgup |
  * `-----------------------------------------------------------------------------------'
  */
 
@@ -87,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|------+------+------+------+------|
  * |      |  \   |  #   |      |  @   |  `   | Play |  F12 |   ^  |  <   |   >  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      | Enter|      |      | Next | Vol- | Vol+ |
+ * |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ |
  * `-----------------------------------------------------------------------------------'
  */
 
@@ -161,12 +160,12 @@ const uint16_t PROGMEM fn_actions[] = { // TODO
   /* Layer switching
   [1] = ACTION_LAYER_TAP_KEY(NAVIGATION_LAYER, KC_SCOLON),
   [2] = ACTION_LAYER_TAP_KEY(NAVIGATION_LAYER, KC_O),
-  [3] = ACTION_LAYER_TAP_KEY(GUI_LAYER, KC_LBRACKET),  
+  [3] = ACTION_LAYER_TAP_KEY(GUI_LAYER, KC_LBRACKET),
   [4] = ACTION_LAYER_TAP_KEY(GUI_LAYER, KC_RBRACKET),
   */
 
   // Modifiers
-  [1] = ACTION_MODS_TAP_KEY(KC_RSFT, KC_ENT), // rshift nejde
+  [1] = ACTION_MODS_TAP_KEY(KC_RSFT, KC_SLSH), // rshift nejde
   //[2] = ACTION_MODS_TAP_KEY(LOWER, KC_SPC) //mysli si ze lower je ctrl
 };
 
